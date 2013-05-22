709ffd8c299be4c96ac3348e4a04412dc241b187
Hack Reactor Students <github@hackreactor.com>
2013-05-22 13:27:42 -0700

    out of time

--- orms/sequelize.js
+++ orms/sequelize.js
@@ -1,1 +1,12 @@
-/* DELETE ME & WRITE YOUR CODE HERE */
+Users = Sequalize.define('users', {
+	username: Sequalize.STRING,
+	userID: {type: Sequalize.INTEGER, autoIncrement: true},
+	firstname: Sequalize.STRING,
+	Lastname: Sequalize.STRING
+});
+
+Tweets = Sequalize.define('tweets', {
+	message: Sequalize.STRING,
+	userID: Sequalize.INTEGER,
+	messageID: {type: Sequalize.INTEGER, autoIncrement: true}
+});
