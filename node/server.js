65ef1c78127c9ac0bf81f68ad363719e5c4de606
Hack Reactor Students <github@hackreactor.com>
2013-05-22 12:13:51 -0700

    not showing image

--- node/server.js
+++ node/server.js
@@ -1,19 +1,32 @@
 var http = require('http'),
     url = require('url'),
     fs = require('fs');
+var defaultCorsHeaders = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10 // Seconds.
+};
+defaultCorsHeaders['Content-Type'] = "plain/tex";
 
 http.createServer(function (request, response) {
   var path = url.parse(request.url, true).pathname;
   switch(request.method) {
+    case 'OPTIONS':
+        response.writeHead(200, defaultCorsHeaders);
+        response.end();
+        break;
     case 'POST':
+      response.writeHead(201, defaultCorsHeaders);
       if (path === '/hifive'){
-        response.end(fs.readFileSync(__dirname + '/assets/hifive.jpg'))
+        response.end(fs.readFileSync(__dirname + '/assets/hifive.jpg'));
       } else if (path === '/lowfive'){
-        response.end(fs.readFileSync(__dirname + '/assets/lowfive.jpg'))
+        response.end(fs.readFileSync(__dirname + '/assets/lowfive.jpg'));
       }
       break;
     case 'GET':
       if(path === '/'){
+        response.writeHead(200, defaultCorsHeaders);
         /* Note: provide a proper status code (200) and content type ('text/html') in the HTTP response  */
         response.end(fs.readFileSync(__dirname+'/index.html'));
       }
